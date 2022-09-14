0b1daa57eb852d06f1df89c47584a0ec8258c0ff
joosthb <joost@hendricksen.net>
2022-09-14 21:26:15 +0200

    switched order to be able to config git

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -11,6 +11,13 @@ ssh -q $1 "sudo apt-get install -y git"
 ssh -q $1 "ssh-keygen -q -t rsa -b 2048 -N \"\" -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub"
 echo "Add key above to github repo as deployment key and press enter..."
 read
+# auto accept fingerprint (unsafe)
+ssh -q $1 "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+# clone repo to default config dir
+ssh -q $1 "git clone git@github.com:joosthb/digital_herbert.git ~/.homeassistant"
+
+cd ~/.homeassistant
+
 echo "Enter github author info for future commits"
 echo "E-mail:"
 read EMAIL
@@ -20,8 +27,3 @@ read FULLNAME
 ssh -q $1 "git config --global user.name \"$FULLNAME\""
 #config default pull strategy
 ssh -q $1 "git config pull.rebase false"
-
-# auto accept fingerprint (unsafe)
-ssh -q $1 "ssh-keyscan github.com >> ~/.ssh/known_hosts"
-# clone repo to default config dir
-ssh -q $1 "git clone git@github.com:joosthb/digital_herbert.git ~/.homeassistant"
