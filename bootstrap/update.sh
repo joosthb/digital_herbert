fe39340a29c02151c864e22c3d422c370cde158e
joosthb <joost@hendricksen.net>
2022-09-19 16:56:02 +0200

    fixed upgrade scripts

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -1,5 +1,7 @@
-#hass
-Stop service
-
+# hass
 /srv/homeassistant/bin/pip3 install --upgrade homeassistant
+sudo systemctl restart home-assistant@homeassistant
 
+# esphome
+/srv/esphome/bin/pip3 install --upgrade esphome
+sudo systemctl restart esphome@homeassistant
