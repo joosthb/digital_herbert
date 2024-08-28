f6951a953eb2e8bb5c7ee27435c24b7da07891b9
Joost <joost@hendricksen.net>
2024-08-28 12:24:08 +0200

    added restore script

---
+++ restore.sh
@@ -0,0 +1,17 @@
+if [ -z $1 ]
+then
+  echo "Please define sourcefile ./restore.sh backupfile.tar"
+  exit 1
+fi
+
+# extract specific files
+tar xvf $1 homeassistant.tar.gz
+
+tar zxvf homeassistant.tar.gz data/home-assistant_v2.db data/media data/www data/custom_components
+
+mv -f data/* ~/.homeassistant/
+
+# cleanup
+rm homeassistant.tar.gz
+rm -rf data
+rm $1
