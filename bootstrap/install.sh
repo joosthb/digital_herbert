ade0360c7b77d0aa2dea0ee3a18dce28458bafdc
Joost Hendricksen <joost@hendricksen.net>
2025-07-23 16:48:55 +0200

    added dnsmasq to netconf and fixed iprange

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -26,11 +26,21 @@ sudo apt-get install -y --no-install-recommends xserver-xorg x11-xserver-utils x
 # browser for showing gui
 sudo apt-get install -y --no-install-recommends chromium-browser
 
+# use dnsmasq
+sudo cp ./templates/00-dnsmasq.conf /etc/NetworkManager/conf.d/
+
+# customize dnsmasq
+sudo cp ./templates/00-custom.conf /etc/NetworkManager/dnsmasq.d/ 
+
+# restart nm
+sudo systemctl restart NetworkManager
+
 # Create local network
 sudo nmcli connection add type wifi ifname wlan0 con-name $AP_SSID autoconnect no ssid $AP_SSID
 sudo nmcli connection modify $AP_SSID 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 sudo nmcli connection modify $AP_SSID wifi-sec.key-mgmt wpa-psk
 sudo nmcli connection modify $AP_SSID wifi-sec.psk "$AP_WPA_PASSPHRASE"
+sudo nmcli connection modify $AP_SSID ipv4.addresses 192.0.99.1/24
 sudo nmcli connection up $AP_SSID
 
 # chrony
